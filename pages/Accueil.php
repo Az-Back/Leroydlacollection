e07c2e61c3a4c8161484b9f900fabefe1c558648
Az-Back <back.ranged@gmail.com>
2022-05-10 15:01:49 +0200

    update

--- pages/Accueil.php
+++ pages/Accueil.php
@@ -8,7 +8,7 @@ $nomDeLaPage = basename(__FILE__);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/index.css">
+    <link rel="stylesheet" href="../styles/accueil.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link href="http://fonts.cdnfonts.com/css/polentical-neon" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
