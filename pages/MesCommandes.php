c8ec1f89c41ac602bc35fcd39be267f73919c716
Az-Back <back.ranged@gmail.com>
2022-05-30 15:56:38 +0200

    update

--- pages/MesCommandes.php
+++ pages/MesCommandes.php
@@ -117,7 +117,7 @@ require("../actions/deleteCommands.php");
                                     <td><?= $Commande['id']; ?></td>
                                     <td><?= $Commande['id_article']; ?></td>
                                     <td><?= $Commande['title']; ?></td>
-                                    <td><?= $Commande['price']; ?> €</td>
+                                    <td><?= number_format($Commande['price'], 2); ?> €</td>
                                     <td><?= $Commande['pseudo_auteur']; ?></td>
                                     <td><?= $Commande['date_buy']; ?></td>
                                     
