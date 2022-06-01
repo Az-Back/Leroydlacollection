5d6ef14565dcd3ae05ab7a438cccbcea9896d32b
Az-Back <back.ranged@gmail.com>
2022-06-01 19:42:35 +0200

    update

--- script/navbar.js
+++ script/navbar.js
@@ -6,6 +6,42 @@ const clear = document.querySelector('.clear');
 
 const text = document.querySelector('#mysearch');
 
+const topText = document.querySelectorAll(".RefNav");
+
+let topNav = document.querySelectorAll(".TextNav");
+
+window.addEventListener('scroll', () =>
+{
+    const scrollTop = document.scrollingElement.scrollTop;
+
+    if(window.innerWidth > 947) {
+    
+        if(scrollTop > 10)
+        {
+            topNav.forEach(back => {
+                back.classList.add("background");
+            })
+            topText.forEach(color => {
+                color.classList.add("color");
+            })
+        }  else {
+            topNav.forEach(back => {
+                back.classList.remove("background");
+            })
+            topText.forEach(color => {
+                color.classList.remove("color");
+            })
+        }
+        }   else if (window.innerHeight < 947 && scrollTop > 1) {
+            topNav.forEach(back => {
+        back.classList.add("background");
+            })
+        }
+       
+});
+
+
+
   // toggle menu script 
   $('.toggle').click(function(){
       $('.List').toggleClass("active");
